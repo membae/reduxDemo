d4890edaa69a5186c14079e62a7f6574bf61208a
Philip <membaphilip1@gmail.com>
2024-12-27 20:50:37 +0300

    index

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('from index.js');
+const redux=require('redux')
+const createStore=redux.createStore
+
+const BUY_CAKE ='BUY_CAKE'
+
+
+function buyCake(){
+    return{
+        type:BUY_CAKE,
+        info:'First redux action'
+    }
+}
+
+//(previousState,action)=>newState
+
+const initialState={
+    numOfCake:10
+}
+
+const reducer=(state=initialState,action)=>{
+    switch(action.type){
+        case BUY_CAKE:return{
+            ...state,//spreading to make a copy
+            numOfCake:state.numOfCake-1
+        }
+        default:return state
+    }
+
+}
+
+const store =createStore(reducer)
+console.log('Initial state',store.getState());
+const unsubscribe =store.subscribe(()=>console.log('Updates state',store.getState()));
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
